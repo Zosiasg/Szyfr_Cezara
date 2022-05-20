3c55d5dbb2c33643333a3b634d12a36561b3a813
Zofia Gerstenkorn <zosiasg@gmail.com>
2022-05-20 19:22:12 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szyfr_Cezara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int przesuniecie;
+            int znak;
+            Console.Write("Podaj przesunięcie: ");
+            przesuniecie = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine("Zacznij wpisywać tekst do zaszyfrowania (q aby zakończyć): ");
+            
+            while ((znak = Console.ReadKey(true).KeyChar) != 'q')
+            {
+                if(znak>='a' && znak<='z') Console.Write(Convert.ToChar((znak - 'a'+ przesuniecie)%26+'a'));
+            }
+                      
+            Console.ReadKey();
+        }
+    }
+}
